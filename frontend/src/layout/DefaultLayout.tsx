0565ac69ab8477a679c227214716f334a0abf9f2
unknown <datp1907@gmail.com>
2024-08-21 20:04:47 +0700

    setup frontend

---
+++ frontend/src/layout/DefaultLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DefaultLayout: React.FC = () => {
+  return (
+    <div>DefaultLayout</div>
+  );
+};
+
+export default DefaultLayout;  // Đảm bảo tên biến ở đây phải khớp với tên biến bạn đã định nghĩa ở trên.
