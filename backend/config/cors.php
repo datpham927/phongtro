653079d6c1beebee76ce1e107b7f2a1fb95884b4
Phạm Ngọc Đạt <103233057+datpham927@users.noreply.github.com>
2024-10-26 21:46:45 +0700

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+  'allowed_origins' => [env('APP_CLIENT_URL', 'http://localhost:5173')],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
