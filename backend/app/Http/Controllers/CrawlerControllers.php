3f81a9b5df32805cc90582bcce8e7546c26f6140
unknown <datp1907@gmail.com>
2024-10-26 21:52:28 +0700

    fix crawl data

--- backend/app/Http/Controllers/CrawlerControllers.php
+++ backend/app/Http/Controllers/CrawlerControllers.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-use App\Models\post;
+use App\Models\Post;
 use App\Models\Post_address;
 use App\Models\Post_area;
 use App\Models\Post_attribute;
@@ -16,12 +16,12 @@ use Symfony\Component\DomCrawler\Crawler;
 class CrawlerControllers extends Controller
 {
    public  $categoryLinks = [
-        // [
-        //     'url' => 'https://phongtro123.com/cho-thue-phong-tro',
-        //     'name' => 'Cho Thuê Phòng Trọ',
-        //     'title' => 'Cho Thuê Phòng Trọ, Giá Rẻ, Tiện Nghi, Mới Nhất 2024',
-        //     'sub_title' => 'Cho thuê phòng trọ - Kênh thông tin số 1 về phòng trọ giá rẻ, phòng trọ sinh viên, phòng trọ cao cấp mới nhất năm 2024. Tất cả nhà trọ cho thuê giá tốt nhất tại Việt Nam.'
-        // ], 
+        [
+            'url' => 'https://phongtro123.com/cho-thue-phong-tro',
+            'name' => 'Cho Thuê Phòng Trọ',
+            'title' => 'Cho Thuê Phòng Trọ, Giá Rẻ, Tiện Nghi, Mới Nhất 2024',
+            'sub_title' => 'Cho thuê phòng trọ - Kênh thông tin số 1 về phòng trọ giá rẻ, phòng trọ sinh viên, phòng trọ cao cấp mới nhất năm 2024. Tất cả nhà trọ cho thuê giá tốt nhất tại Việt Nam.'
+        ], 
         //   [
         //     'url' => 'https://phongtro123.com/nha-cho-thue',
         //     'name' => 'Cho Thuê Nhà Nguyên Căn',
@@ -41,12 +41,12 @@ class CrawlerControllers extends Controller
         //     'title' => 'Tìm Người Ở Ghép, Tìm Nam Ở Ghép, Tìm Nữ Ở Ghép, Mới Nhất 2024',
         //     'sub_title' => 'Tìm người ở ghép, tìm nam ở ghép, tìm nữ ở ghép, share phòng trọ, tìm chỗ ở ghép cùng, tìm bạn ở ghép, xin ở ghép mới nhất 2024. Đăng tin ở ghép hiệu quả, nhanh chóng nhất...'
         // ],
-        [
-            'url' => 'https://phongtro123.com/cho-thue-can-ho',
-            'name' => 'Cho Thuê Căn Hộ',
-            'title' => 'Cho Thuê Căn Hộ Chung Cư, Giá Rẻ, View Đẹp, Mới Nhất 2024',
-            'sub_title' => 'Cho thuê căn hộ - Kênh đăng tin cho thuê căn hộ số 1: giá rẻ, chính chủ, đầy đủ tiện nghi. Cho thuê chung cư với nhiều mức giá, diện tích cho thuê khác nhau.'
-        ], 
+        // [
+        //     'url' => 'https://phongtro123.com/cho-thue-can-ho',
+        //     'name' => 'Cho Thuê Căn Hộ',
+        //     'title' => 'Cho Thuê Căn Hộ Chung Cư, Giá Rẻ, View Đẹp, Mới Nhất 2024',
+        //     'sub_title' => 'Cho thuê căn hộ - Kênh đăng tin cho thuê căn hộ số 1: giá rẻ, chính chủ, đầy đủ tiện nghi. Cho thuê chung cư với nhiều mức giá, diện tích cho thuê khác nhau.'
+        // ], 
     ];
     
     public $users=[
