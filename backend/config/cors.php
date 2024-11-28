10af2319cf51f1ed82b1cfc81c8ed7357cbc54e6
unknown <datp1907@gmail.com>
2024-11-28 19:11:17 +0700

    update core

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('APP_CLIENT_URL', 'http://localhost:5173')],
+    'allowed_origins' => [env('APP_CLIENT_URL', 'http://localhost:5173'),'http://localhost:5173'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
